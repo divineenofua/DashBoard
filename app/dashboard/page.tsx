b9b6a1fd796796d1567fec4f33d437aeb07fdf7e
divineenofua <divineenofua@gmail.com>
2024-04-01 18:29:38 +0100

    first commit of DashBoard interface

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,4 @@
+
+export default function Page() {
+    return <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque illum deserunt natus, nihil velit ab aspernatur quibusdam eius doloribus, dignissimos fugit, dolore blanditiis necessitatibus ipsam magni temporibus! Rem, excepturi dolor.</p>;
+  }
